b6bf4e6356b1e60c2e7dd616c68a474be5800953
Ritika Jain <ritzjain25@gmail.com>
2019-08-20 16:25:11 +0530

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+package com.training;
+public class App{
+	public static void main(String[] ar)
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package com.training;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest{
+	@Test
+	public void stringTest()
+	{
+		assertTrue(true);
+	}
+}
